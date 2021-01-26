3c36f10f099642758553ea962421545aeaf93720
john <john@johng.cn>
2021-01-26 21:33:15 +0800

    up

---
+++ go.mod
@@ -0,0 +1,14 @@
+module gftracing
+
+go 1.11
+
+require (
+	github.com/gogf/gcache-adapter v0.0.4-0.20210126062229-c84b9cefa528
+	github.com/gogf/gf v1.15.2-0.20210126085339-24e2c7926e39
+	github.com/gogo/protobuf v1.3.2
+	github.com/golang/protobuf v1.4.3
+	go.opentelemetry.io/otel v0.16.0
+	go.opentelemetry.io/otel/exporters/trace/jaeger v0.16.0
+	go.opentelemetry.io/otel/sdk v0.16.0
+	google.golang.org/grpc v1.35.0
+)
---
+++ tracing/tracing_interceptor_server.go
@@ -0,0 +1,97 @@
+package tracing
+
+import (
+	"context"
+	"go.opentelemetry.io/otel/baggage"
+	"go.opentelemetry.io/otel/codes"
+	"go.opentelemetry.io/otel/trace"
+	"google.golang.org/grpc"
+	grpcCodes "google.golang.org/grpc/codes"
+	"google.golang.org/grpc/metadata"
+	"google.golang.org/grpc/status"
+)
+
+// UnaryServerInterceptor returns a grpc.UnaryServerInterceptor suitable
+// for use in a grpc.NewServer call.
+func UnaryServerInterceptor(
+	ctx context.Context,
+	req interface{},
+	info *grpc.UnaryServerInfo,
+	handler grpc.UnaryHandler) (interface{}, error) {
+	requestMetadata, _ := metadata.FromIncomingContext(ctx)
+	metadataCopy := requestMetadata.Copy()
+
+	entries, spanCtx := Extract(ctx, &metadataCopy)
+	ctx = baggage.ContextWithValues(ctx, entries...)
+
+	tracer := newConfig(nil).TracerProvider.Tracer(
+		"gftracing.GrpcServer",
+		trace.WithInstrumentationVersion("0.1.0"),
+	)
+
+	name, attr := spanInfo(info.FullMethod, peerFromCtx(ctx))
+	ctx, span := tracer.Start(
+		trace.ContextWithRemoteSpanContext(ctx, spanCtx),
+		name,
+		trace.WithSpanKind(trace.SpanKindServer),
+		trace.WithAttributes(attr...),
+	)
+	defer span.End()
+
+	messageReceived.Event(ctx, 1, req)
+
+	resp, err := handler(ctx, req)
+	if err != nil {
+		s, _ := status.FromError(err)
+		span.SetStatus(codes.Error, s.Message())
+		span.SetAttributes(statusCodeAttr(s.Code()))
+		messageSent.Event(ctx, 1, s.Proto())
+	} else {
+		span.SetAttributes(statusCodeAttr(grpcCodes.OK))
+		messageSent.Event(ctx, 1, resp)
+	}
+
+	return resp, err
+}
+
+// StreamServerInterceptor returns a grpc.StreamServerInterceptor suitable
+// for use in a grpc.NewServer call.
+func StreamServerInterceptor(
+	srv interface{},
+	ss grpc.ServerStream,
+	info *grpc.StreamServerInfo,
+	handler grpc.StreamHandler) error {
+	ctx := ss.Context()
+
+	requestMetadata, _ := metadata.FromIncomingContext(ctx)
+	metadataCopy := requestMetadata.Copy()
+
+	entries, spanCtx := Extract(ctx, &metadataCopy)
+	ctx = baggage.ContextWithValues(ctx, entries...)
+
+	tracer := newConfig(nil).TracerProvider.Tracer(
+		"gftracing.GrpcServer",
+		trace.WithInstrumentationVersion("0.1.0"),
+	)
+
+	name, attr := spanInfo(info.FullMethod, peerFromCtx(ctx))
+	ctx, span := tracer.Start(
+		trace.ContextWithRemoteSpanContext(ctx, spanCtx),
+		name,
+		trace.WithSpanKind(trace.SpanKindServer),
+		trace.WithAttributes(attr...),
+	)
+	defer span.End()
+
+	err := handler(srv, wrapServerStream(ctx, ss))
+
+	if err != nil {
+		s, _ := status.FromError(err)
+		span.SetStatus(codes.Error, s.Message())
+		span.SetAttributes(statusCodeAttr(s.Code()))
+	} else {
+		span.SetAttributes(statusCodeAttr(grpcCodes.OK))
+	}
+
+	return err
+}
