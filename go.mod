84c3cdec8fff0a69d41607a43165bc37f9a0eba7
john <john@johng.cn>
2021-03-29 18:13:28 +0800

    up

--- go.mod
+++ go.mod
@@ -4,13 +4,13 @@ go 1.11
 
 require (
 	github.com/gogf/gcache-adapter v0.1.0
-	github.com/gogf/gf v1.15.5-0.20210311152939-41f2138b3938
-	github.com/gogf/katyusha v0.1.2-0.20210203072924-61d9cc2addf5
+	github.com/gogf/gf v1.15.5-0.20210329100547-d4e4b9addf1b
+	github.com/gogf/katyusha v0.1.2-0.20210329092602-30e9a2325ace
 	github.com/gogo/protobuf v1.3.2
 	github.com/golang/protobuf v1.4.3
-	go.opentelemetry.io/otel/exporters/trace/jaeger v0.16.0
-	go.opentelemetry.io/otel/sdk v0.16.0
-	google.golang.org/grpc v1.35.0
+	go.opentelemetry.io/otel/exporters/trace/jaeger v0.19.0
+	go.opentelemetry.io/otel/sdk v0.19.0
+	google.golang.org/grpc v1.36.0
 )
 
 replace (
--- tracing/tracing_jaeger.go
+++ tracing/tracing_jaeger.go
@@ -1,9 +1,15 @@
 package tracing
 
 import (
+	"github.com/gogf/gf/os/genv"
 	"go.opentelemetry.io/otel/exporters/trace/jaeger"
 	"go.opentelemetry.io/otel/sdk/trace"
 	"strings"
+)
+
+const (
+	// The service name.
+	jaegerEnvServiceName = "JAEGER_SERVICE_NAME"
 )
 
 // InitJaeger initializes and registers jaeger to global TracerProvider.
@@ -19,13 +25,14 @@ func InitJaeger(serviceName, endpoint string) (flush func(), err error) {
 		// UDP.
 		endpointOption = jaeger.WithAgentEndpoint(endpoint)
 	}
+	if err := genv.Set(jaegerEnvServiceName, serviceName); err != nil {
+		return nil, err
+	}
 	return jaeger.InstallNewPipeline(
 		endpointOption,
-		jaeger.WithProcess(jaeger.Process{
-			ServiceName: serviceName,
-		}),
-		jaeger.WithSDK(&trace.Config{
-			DefaultSampler: trace.AlwaysSample(),
-		}),
+		jaeger.WithProcessFromEnv(),
+		jaeger.WithSDKOptions(
+			trace.WithSampler(trace.AlwaysSample()),
+		),
 	)
 }
